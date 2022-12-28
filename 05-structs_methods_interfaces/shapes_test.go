82259729fb5407db33caa174c1a26f02077455c4
Tomás R. Pita <tomasrpita@gmail.com>
2022-12-28 07:03:19 +0100

    Adds documentation to shapes.go

--- 05-structs_methods_interfaces/shapes_test.go
+++ 05-structs_methods_interfaces/shapes_test.go
@@ -12,33 +12,6 @@ func TestPerimeter(t *testing.T) {
 	}
 }
 
-// func TestArea(t *testing.T) {
-
-// 	checkArea := func(t testing.TB, shape Shape, want float64) {
-
-// 		t.Helper()
-// 		got := shape.Area()
-// 		if got != want {
-// 			t.Errorf("got %g want %g", got, want)
-// 		}
-
-// 	}
-
-// 	t.Run("rectangles", func(t *testing.T) {
-// 		rectangle := Rectangle{12.0, 6.0}
-// 		want := 72.0
-
-// 		checkArea(t, rectangle, want)
-// 	})
-
-// 	t.Run("circles", func(t *testing.T) {
-// 		circle := Circle{10}
-// 		want := 314.1592653589793
-
-// 		checkArea(t, circle, want)
-// 	})
-
-// }
 func TestArea(t *testing.T) {
 	areaTests := []struct {
 		name    string
