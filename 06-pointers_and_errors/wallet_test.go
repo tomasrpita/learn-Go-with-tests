2cf0c3a2be48806ae521087502ed7bccdf894b06
Tomás R. Pita <tomasrpita@gmail.com>
2022-12-28 08:09:29 +0100

    Add Wallet with methods Balance and Deposit

--- 06-pointers_and_errors/wallet_test.go
+++ 06-pointers_and_errors/wallet_test.go
@@ -1,1 +1,20 @@
 package wallet
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+	wallet := Wallet{}
+	wallet.Deposit(Bitcoin(10))
+
+	got := wallet.Balance()
+	want := Bitcoin(11)
+
+	fmt.Printf("address of balance in test is %v \n", &wallet.balance)
+
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
