0a96c7af8f0b23fca3dc697efb0a90f5761dc30c
Tomás R. Pita <tomasrpita@gmail.com>
2022-12-29 13:24:45 +0100

    Adds doc to 07-maps

--- 07-maps/dictionary.go
+++ 07-maps/dictionary.go
@@ -1,19 +1,25 @@
 package dictionary
 
 const (
-	ErrNotFound      = DictionaryErr("could not find the word you were looking for")
-	ErrWordExists    = DictionaryErr("cannot add word because it already exists")
+	// ErrNotFound means the definition could not be found for the given word.
+	ErrNotFound = DictionaryErr("could not find the word you were looking for")
+	// ErrWordExists means you are trying to add a word that is already known.
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+	// ErrWordExists occurs when trying to update a word not in the dictionary.
 	ErrWordDoesFound = DictionaryErr("cannot update word because it does not exist")
 )
 
+// DictionaryErr are errors that can happen when interacting with the dictionary.
 type DictionaryErr string
 
 func (e DictionaryErr) Error() string {
 	return string(e)
 }
 
+// Dictionary store definitions to words.
 type Dictionary map[string]string
 
+// DictionaryErr are errors that can happen when interacting with the dictionary.
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
 	if !ok {
@@ -23,6 +29,7 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
+// Add inserts a word and definition into the dictionary.
 func (d Dictionary) Add(word, definition string) error {
 	_, ok := d[word]
 	if ok {
@@ -32,6 +39,7 @@ func (d Dictionary) Add(word, definition string) error {
 	return nil
 }
 
+// Update changes the definition of a given word.
 func (d Dictionary) Update(word, definition string) error {
 	_, ok := d[word]
 	if !ok {
@@ -42,6 +50,7 @@ func (d Dictionary) Update(word, definition string) error {
 	return nil
 }
 
+// Delete removes a word from the dictionary.
 func (d Dictionary) Delete(word string) {
 	delete(d, word)
 }
