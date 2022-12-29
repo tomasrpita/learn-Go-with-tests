837c3e1763570f53d7959b6a5f5d10569f0e5909
Tomás R. Pita <tomasrpita@gmail.com>
2022-12-29 07:29:30 +0100

    Add Dictionary type with methds Search and Add

---
+++ 07-maps/dictionary_test.go
@@ -0,0 +1,62 @@
+package dictionary
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+	dict := Dictionary{"test": "this is just a test"}
+
+	t.Run("Know word", func(t *testing.T) {
+
+		got, _ := dict.Search("test")
+		want := "this is just a test"
+
+		assertString(t, got, want)
+
+	})
+
+	t.Run("Unknow word", func(t *testing.T) {
+		_, got := dict.Search("unknow")
+
+		assertError(t, got, ErrNotFound)
+	})
+}
+
+func TestAdd(t *testing.T) {
+
+	t.Run("new word", func(t *testing.T) {
+		dict := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
+
+		dict.Add(word, definition)
+
+		assertDefinition(t, dict, word, definition)
+	})
+
+}
+
+func assertString(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q given, %q", got, want, "test")
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
+	}
+
+}
+
+func assertDefinition(t testing.TB, dict Dictionary, word, definition string) {
+	t.Helper()
+	got, err := dict.Search(word)
+	if err != nil {
+		t.Fatal("Should find added word:", err)
+	}
+	if got != definition {
+		t.Errorf("got %q want %q", got, definition)
+	}
+}
