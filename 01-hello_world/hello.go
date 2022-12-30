dcf9618d87c37fe7348d52022ff52b6bcbee038c
Tomás R. Pita <tomasrpita@gmail.com>
2022-12-30 07:27:05 +0100

    add doc to chapter 01

--- 01-hello_world/hello.go
+++ 01-hello_world/hello.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 )
 
+// consts for languaues and greetings
 const spanish = "Spanish"
 const french = "French"
 const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
 const frenchHelloPrefix = "Bonjour, "
 
+// Hello returns a personalized greeting in a given languague
 func Hello(name string, language string) string {
 
 	if name == "" {
@@ -19,6 +21,7 @@ func Hello(name string, language string) string {
 	return greetingPrefix(language) + name + "!"
 }
 
+// greetingPrefix match the languague with the corresponding prefix
 func greetingPrefix(language string) (prefix string) {
 
 	switch language {
