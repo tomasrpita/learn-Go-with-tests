0f177ad2a2ad075e2bb27927312fc8a360edd45b
Tomás R. Pita <tomasrpita@gmail.com>
2023-01-03 08:05:47 +0100

    Adds the Racer fucntion, missing only the last requirement

--- 11-select/racer.go
+++ 11-select/racer.go
@@ -1,1 +1,37 @@
 package racer
+
+import "net/http"
+
+// func Racer(a, b string) (winner string) {
+// 	aDuration := measureResponseTime(a)
+// 	bDuration := measureResponseTime(b)
+
+// 	if aDuration > bDuration {
+// 		return b
+// 	}
+// 	return a
+// }
+
+// func measureResponseTime(url string) time.Duration {
+// 	startA := time.Now()
+// 	http.Get(url)
+// 	return time.Since(startA)
+// }
+
+func Racer(a, b string) (winner string) {
+	select {
+	case <-ping(a):
+		return a
+	case <-ping(b):
+		return b
+	}
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
+}
--- 11-select/racer_test.go
+++ 11-select/racer_test.go
@@ -1,1 +1,34 @@
 package racer
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+	slowServer := makeDelayServer(20 * time.Millisecond)
+	fastServer := makeDelayServer(1 * time.Millisecond)
+
+	defer slowServer.Close()
+	defer fastServer.Close()
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
+
+	want := fastURL
+	got := Racer(slowURL, fastURL)
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+
+}
+
+func makeDelayServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+}
