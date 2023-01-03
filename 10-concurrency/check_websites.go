d728b93b789fb2cd523b852e9bbd37be4afc9287
Tomás R. Pita <tomasrpita@gmail.com>
2023-01-03 06:38:36 +0100

    Add dic to ch 10 check_websites

--- 10-concurrency/check_websites.go
+++ 10-concurrency/check_websites.go
@@ -1,11 +1,14 @@
 package concurrency
 
+// WebsiteChequer cheacks a url, returning a bool.
 type Websitechequer func(string) bool
 type result struct {
 	string
 	bool
 }
 
+// CheckWebsites takes a WebsiteChequer and a slice of url and returns a map
+// of urls to the result of checking each url with the  WebsiteChecker funtion.
 func CheckWebsites(wc Websitechequer, urls []string) map[string]bool {
 	results := make(map[string]bool)
 	resultChannel := make(chan result)
