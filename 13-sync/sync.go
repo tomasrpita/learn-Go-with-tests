3eb093af4a342e273bf11b0745e429fd1c27f53c
Tomás R. Pita <tomasrpita@gmail.com>
2023-01-05 14:29:18 +0100

    Add doc to Counter

--- 13-sync/sync.go
+++ 13-sync/sync.go
@@ -2,21 +2,25 @@ package syncronicity
 
 import "sync"
 
+// Counter is a simple counter.
 type Counter struct {
-	mu    sync.Mutex
+	mu    sync.Mutex // guards value
 	value int
 }
 
+// Inc increments the counter.
 func (c *Counter) Inc() {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	c.value++
 }
 
+// Value returns the current value of the counter.
 func (c *Counter) Value() int {
 	return c.value
 }
 
+// NewCounter returns a new Counter.
 func NewCounter() *Counter {
 	return &Counter{}
 }
