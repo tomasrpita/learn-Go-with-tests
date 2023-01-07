b70b84323de6e4bffe479dff1d93929df54e0715
Tomás R. Pita <tomasrpita@gmail.com>
2023-01-07 10:29:02 +0100

    Change package name, and add happy test

--- 14-context/context_test.go
+++ 14-context/context_test.go
@@ -1,1 +1,29 @@
-package context
+package server
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type StubStore struct {
+	response string
+}
+
+func (s *StubStore) Fetch() string {
+	return s.response
+}
+
+func TestServer(t *testing.T) {
+	data := "Hello, world"
+	svr := Server(&StubStore{data})
+
+	request := httptest.NewRequest(http.MethodGet, "/", nil)
+	response := httptest.NewRecorder()
+
+	svr.ServeHTTP(response, request)
+
+	if response.Body.String() != data {
+		t.Errorf("got %q, want %q", response.Body.String(), data)
+	}
+}
--- 14-context/go.mod
+++ 14-context/go.mod
@@ -1,3 +1,3 @@
-module context
+module server
 
 go 1.19
