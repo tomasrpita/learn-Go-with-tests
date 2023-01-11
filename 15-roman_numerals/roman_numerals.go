91b911924adfa1f0ed97e70bae30ccb1cdf19002
Tomás R. Pita <tomasrpita@gmail.com>
2023-01-11 06:07:54 +0100

    ConvertToRoman works up to number 39

--- 15-roman_numerals/roman_numerals.go
+++ 15-roman_numerals/roman_numerals.go
@@ -5,21 +5,12 @@ import "strings"
 func ConvertToRoman(arabic int) string {
 
 	var result strings.Builder
-	// for i := 0; i < arabic; i++ {
-	// 	if arabic == 5 {
-	// 		result.WriteString("V")
-	// 		break
-	// 	}
-	// 	if arabic == 4 {
-	// 		result.WriteString("IV")
-	// 		break
-	// 	}
-
-	// 	result.WriteString("I")
-	// }
 
 	for arabic > 0 {
 		switch {
+		case arabic > 9:
+			result.WriteString("X")
+			arabic -= 10
 		case arabic > 8:
 			result.WriteString("IX")
 			arabic -= 9
