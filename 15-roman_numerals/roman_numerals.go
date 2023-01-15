f23e8a2040eaad1062dc25d2532cab6012f53dc5
Tomás R. Pita <tomasrpita@gmail.com>
2023-01-15 07:09:21 +0100

    Refactoring ch 15, ConvertToArabic

--- 15-roman_numerals/roman_numerals.go
+++ 15-roman_numerals/roman_numerals.go
@@ -29,13 +29,24 @@ func (rn RomanNumerals) ValueOf(symbols ...byte) int {
 
 	symbol := string(symbols)
 
-	for i := 0; i < len(rn); i++ {
-		if symbol == rn[i].Symbol {
-			return rn[i].Value
+	for _, a := range rn {
+		if a.Symbol == symbol {
+			return a.Value
 		}
 	}
 
 	return 0
+}
+
+// Exists - We are using it to prove the existence of subtractive groups, i.e: IV, CX
+func (r RomanNumerals) Exists(symbols ...byte) bool {
+	symbol := string(symbols)
+	for _, s := range r {
+		if s.Symbol == symbol {
+			return true
+		}
+	}
+	return false
 }
 
 func ConvertToRoman(arabic int) string {
@@ -52,33 +63,31 @@ func ConvertToRoman(arabic int) string {
 
 }
 
-func ConvertToArabic(roman string) int {
-	total := 0
-
-	for i := 0; i < len(roman); i++ {
-		symbol := roman[i] // it is a byte
-
-		// look ahead to next symbol if we can and, the current symbol is base 10 (Only valid substrators)
-		if couldBeSubstractive(i, roman, symbol) {
-			nextSymbol := roman[i+1]
-
-			// get the value of the two characater string
-			if value := allRomanNumerals.ValueOf(symbol, nextSymbol); value != 0 {
-				total += value
-				i++ // move past this character too for next loop
-			} else {
-				total += allRomanNumerals.ValueOf(symbol)
-			}
-		} else {
-			total += allRomanNumerals.ValueOf(symbol)
-		}
+func ConvertToArabic(roman string) (total int) {
+	for _, symbols := range windowedRoman(roman).Symbols() {
+		total += allRomanNumerals.ValueOf(symbols...)
 	}
-	return total
+	return
 
 }
 
-func couldBeSubstractive(index int, roman string, currentSymbol byte) bool {
-	isSubstractive := currentSymbol == 'I' || currentSymbol == 'X' || currentSymbol == 'C'
+type windowedRoman string
 
-	return index+1 < len(roman) && isSubstractive
+func (w windowedRoman) Symbols() (symbols [][]byte) {
+	for i := 0; i < len(w); i++ {
+		symbol := w[i]
+		notAtEnd := i+1 < len(w)
+
+		if notAtEnd && isSubtractive(symbol) && allRomanNumerals.Exists(symbol, w[i+1]) {
+			symbols = append(symbols, []byte{symbol, w[i+1]})
+			i++
+		} else {
+			symbols = append(symbols, []byte{symbol})
+		}
+	}
+	return
+}
+
+func isSubtractive(symbol byte) bool {
+	return symbol == 'I' || symbol == 'X' || symbol == 'C'
 }
