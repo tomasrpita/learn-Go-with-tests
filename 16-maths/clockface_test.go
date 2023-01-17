4ac9fefbcfed12f9856e169bd4f1a45b939641c4
Tomás R. Pita <tomasrpita@gmail.com>
2023-01-17 06:06:01 +0100

    Add func secondsInRadians

--- 16-maths/clockface_test.go
+++ 16-maths/clockface_test.go
@@ -1,18 +1,41 @@
 package clockface
 
 import (
+	"math"
 	"testing"
 	"time"
 )
 
-func TestSecondHanfMidnight(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+// func TestSecondHanfMidnight(t *testing.T) {
+// 	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
 
-	want := Point{X: 150, Y: 150 - 90}
-	got := SecondHand(tm)
+// 	want := Point{X: 150, Y: 150 - 90}
+// 	got := SecondHand(tm)
 
-	if got != want {
-		t.Errorf("Got %v, wanted %v", got, want)
+// 	if got != want {
+// 		t.Errorf("Got %v, wanted %v", got, want)
+// 	}
+
+// }
+
+// func TestSecondHAdAt30Seconds(t *testing.T) {
+// 	tm := time.Date(1377, time.January, 1, 0, 0, 30, 0, time.UTC)
+
+// 	want := Point{X: 150, Y: 150 + 90}
+// 	got := SecondHand(tm)
+
+// 	if got != want {
+// 		t.Errorf("Got %v, wanted %v", got, want)
+// 	}
+// }
+
+func TestSecondsInRadians(t *testing.T) {
+	thirtySeconds := time.Date(1377, time.January, 1, 0, 0, 30, 0, time.UTC)
+	want := math.Pi
+	got := secondsInRadians(thirtySeconds)
+
+	if want != got {
+		t.Fatalf("Wanted %v radians, but got %v", want, got)
 	}
 
 }
