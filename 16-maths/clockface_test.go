d4acda80f6139f9bd1ddd19b0cc56494819f60b6
Tomás R. Pita <tomasrpita@gmail.com>
2023-01-18 06:02:14 +0100

    Add TestSecondsInRadians

--- 16-maths/clockface_test.go
+++ 16-maths/clockface_test.go
@@ -29,13 +29,43 @@ import (
 // 	}
 // }
 
-func TestSecondsInRadians(t *testing.T) {
-	thirtySeconds := time.Date(1377, time.January, 1, 0, 0, 30, 0, time.UTC)
-	want := math.Pi
-	got := secondsInRadians(thirtySeconds)
+// func TestSecondsInRadians(t *testing.T) {
+// 	thirtySeconds := time.Date(1377, time.January, 1, 0, 0, 30, 0, time.UTC)
+// 	want := math.Pi
+// 	got := secondsInRadians(thirtySeconds)
 
-	if want != got {
-		t.Fatalf("Wanted %v radians, but got %v", want, got)
+// 	if want != got {
+// 		t.Fatalf("Wanted %v radians, but got %v", want, got)
+// 	}
+
+// }
+
+func TestSecondsInRadians(t *testing.T) {
+	cases := []struct {
+		time  time.Time
+		angle float64
+	}{
+		{simpleTime(0, 0, 30), math.Pi},
+		{simpleTime(0, 0, 0), math.Pi},
+		{simpleTime(0, 0, 45), (math.Pi / 2) * 3},
+		{simpleTime(0, 0, 30), (math.Pi / 30) * 7},
 	}
 
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			got := secondsInRadians(c.time)
+			if got != c.angle {
+				t.Fatalf("Wanted %v radians, but got %v", c.angle, got)
+			}
+		})
+	}
+
+}
+
+func simpleTime(hours, minutes, seconds int) time.Time {
+	return time.Date(1492, time.October, 12, hours, minutes, seconds, 0, time.UTC)
+}
+
+func testName(t time.Time) string {
+	return t.Format("15:04:05")
 }
