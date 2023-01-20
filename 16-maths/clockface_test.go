26297a8bf99aa4b0c7de52c66c0241ba8c68f4c4
Tomás R. Pita <tomasrpita@gmail.com>
2023-01-20 05:58:41 +0100

    Fix SecondHand func

--- 16-maths/clockface_test.go
+++ 16-maths/clockface_test.go
@@ -8,9 +8,9 @@ import (
 
 // Acceptance test
 func TestSecondHanfMidnight(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
 
-	want := Point{X: 150, Y: 150 - 90}
+	want := Point{X: 150, Y: 150 + 90}
 	got := SecondHand(tm)
 
 	if got != want {
