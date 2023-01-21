47d0620121e922a74bd729d568a76b22fa23b856
Tomás R. Pita <tomasrpita@gmail.com>
2023-01-21 10:01:57 +0100

    Add main.go

--- 16-maths/clockface.go
+++ 16-maths/clockface.go
@@ -5,6 +5,10 @@ import (
 	"time"
 )
 
+const secondHandLength = 90
+const clockCentreX = 150
+const clockCentreY = 150
+
 // A Point represents a two dimensional Cartesian coordinate.
 type Point struct {
 	X float64
@@ -15,9 +19,9 @@ type Point struct {
 // represented as a Point.
 func SecondHand(t time.Time) Point {
 	p := secondHandPoint(t)
-	p = Point{p.X * 90, p.Y * 90}   // scale
-	p = Point{p.X, -p.Y}            // flip
-	p = Point{p.X + 150, p.Y + 150} //
+	p = Point{p.X * secondHandLength, p.Y * secondHandLength} // scale
+	p = Point{p.X, -p.Y}                                      // flip
+	p = Point{p.X + clockCentreX, p.Y + clockCentreY}         // translate
 	return p
 }
 
