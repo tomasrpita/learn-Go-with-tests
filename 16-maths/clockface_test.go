1d18dd0667b3cd6d4d728ae55fe66fc42497bc77
Tomás R. Pita <tomasrpita@gmail.com>
2023-01-24 06:07:01 +0100

    Add SVGWriter

--- 16-maths/clockface_test.go
+++ 16-maths/clockface_test.go
@@ -6,41 +6,6 @@ import (
 	"time"
 )
 
-// Acceptance test
-func TestSecondHanfMidnight(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
-
-	want := Point{X: 150, Y: 150 + 90}
-	got := SecondHand(tm)
-
-	if got != want {
-		t.Errorf("Got %v, wanted %v", got, want)
-	}
-
-}
-
-// func TestSecondHAdAt30Seconds(t *testing.T) {
-// 	tm := time.Date(1377, time.January, 1, 0, 0, 30, 0, time.UTC)
-
-// 	want := Point{X: 150, Y: 150 + 90}
-// 	got := SecondHand(tm)
-
-// 	if got != want {
-// 		t.Errorf("Got %v, wanted %v", got, want)
-// 	}
-// }
-
-// func TestSecondsInRadians(t *testing.T) {
-// 	thirtySeconds := time.Date(1377, time.January, 1, 0, 0, 30, 0, time.UTC)
-// 	want := math.Pi
-// 	got := secondsInRadians(thirtySeconds)
-
-// 	if want != got {
-// 		t.Fatalf("Wanted %v radians, but got %v", want, got)
-// 	}
-
-// }
-
 func TestSecondsInRadians(t *testing.T) {
 	cases := []struct {
 		time  time.Time
