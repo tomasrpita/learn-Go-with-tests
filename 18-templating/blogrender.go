3694c27442c22cf59dbb8b835693eabafe869e65
Tomás R. Pita <tomasrrpita@gmail.com>
2023-02-09 15:07:38 +0100

    Add type Post and init Render func

--- 18-templating/blogrender.go
+++ 18-templating/blogrender.go
@@ -1,1 +1,12 @@
 package blogrender
+
+import "io"
+
+type Post struct {
+	Title, Body, Description string
+	Tags                     []string
+}
+
+func Render(w io.Writer, p Post) error {
+	return nil
+}
--- 18-templating/blogrender_test.go
+++ 18-templating/blogrender_test.go
@@ -1,1 +1,33 @@
 package blogrender_test
+
+import (
+	"bytes"
+	blogrender "learn-Go-with-tests/18-templating"
+	"testing"
+)
+
+func TestRender(t *testing.T) {
+	var (
+		aPost = blogrender.Post{
+			Title:       "hello world",
+			Body:        "This is a post",
+			Description: "This is a description",
+			Tags:        []string{"go", "tdd"},
+		}
+	)
+
+	t.Run("it converts a single post into HTML", func(t *testing.T) {
+		buf := bytes.Buffer{}
+		err := blogrender.Render(&buf, aPost)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		got := buf.String()
+		want := `<h1>hello world</h1>`
+		if got != want {
+			t.Errorf("got '%s' want  '%s'", got, want)
+		}
+	})
+}
