9a6d6c317c6ac327f09691e4556178dc2a54c638
Tomás R. Pita <tomasrrpita@gmail.com>
2023-02-09 17:14:27 +0100

    Add description and Tags to render func

--- 18-templating/blogrender.go
+++ 18-templating/blogrender.go
@@ -11,6 +11,22 @@ type Post struct {
 }
 
 func Render(w io.Writer, p Post) error {
-	_, err := fmt.Fprintf(w, "<h1>%s</h1>", p.Title)
+	_, err := fmt.Fprintf(w, "<h1>%s</h1>\n<p>%s</p>\nTags: <ul>", p.Title, p.Description)
+	if err != nil {
+		return err
+	}
+
+	for _, tag := range p.Tags {
+		_, err := fmt.Fprintf(w, "<li>%s</li>", tag)
+		if err != nil {
+			return err
+		}
+	}
+
+	_, err = fmt.Fprint(w, "</ul>")
+	if err != nil {
+		return err
+	}
+
 	return err
 }
