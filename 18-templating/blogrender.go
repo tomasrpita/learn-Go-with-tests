8eb409e7f73ce2b1f63aa27adb12c23d61784821
Tomás R. Pita <tomasrpita@gmail.com>
2023-02-12 07:12:21 +0100

    Add PostRender type and change func Render to PostRender.Render meth

--- 18-templating/blogrender.go
+++ 18-templating/blogrender.go
@@ -16,13 +16,21 @@ type Post struct {
 	Tags                     []string
 }
 
-func Render(w io.Writer, p Post) error {
+type PostRenderer struct {
+	templ *template.Template
+}
+
+func NewPostRenderer() (*PostRenderer, error) {
 	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
 	if err != nil {
-		return err
+		return nil, err
 	}
+	return &PostRenderer{templ: templ}, nil
+}
 
-	if err := templ.Execute(w, p); err != nil {
+func (r *PostRenderer) Render(w io.Writer, p Post) error {
+
+	if err := r.templ.Execute(w, p); err != nil {
 		return err
 	}
 
--- 18-templating/blogrender_test.go
+++ 18-templating/blogrender_test.go
@@ -2,29 +2,47 @@ package blogrender_test
 
 import (
 	"bytes"
+	"io"
 	blogrender "learn-Go-with-tests/18-templating"
 	"testing"
 
 	approvals "github.com/approvals/go-approval-tests"
 )
 
+var (
+	aPost = blogrender.Post{
+		Title:       "hello world",
+		Body:        "This is a post",
+		Description: "This is a description",
+		Tags:        []string{"go", "tdd"},
+	}
+)
+
 func TestRender(t *testing.T) {
-	var (
-		aPost = blogrender.Post{
-			Title:       "hello world",
-			Body:        "This is a post",
-			Description: "This is a description",
-			Tags:        []string{"go", "tdd"},
-		}
-	)
+
+	postRederer, err := blogrender.NewPostRenderer()
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	t.Run("it converts a single post into HTML", func(t *testing.T) {
 		buf := bytes.Buffer{}
 
-		if err := blogrender.Render(&buf, aPost); err != nil {
+		if err := postRederer.Render(&buf, aPost); err != nil {
 			t.Fatal(err)
 		}
 
 		approvals.VerifyString(t, buf.String())
 	})
+}
+
+func BenchmarkXxx(b *testing.B) {
+	postRederer, err := blogrender.NewPostRenderer()
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		postRederer.Render(io.Discard, aPost)
+	}
 }
